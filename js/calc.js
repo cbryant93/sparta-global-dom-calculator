800cd7a3715b43bea0fa69a76de7105020e124fc
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-01-31 21:13:16 +0000

    basic program structure

---
+++ js/calc.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('This code was run when the page is finished loading');
+});
+
+var screen = document.getElementById('screen');
+var buttons = document.getElementsByClassName('buttonNum');
+var operator = document.getElementsByClassName('operator');
+var clear = document.getElementsByClassName('buttonClear');
+var sum = document.getElementsByClassName('equals');
+
+function displayData(text){
+ return  screen.innerHTML = text;
+}
+
+for (i = 0; i < buttons.length; i++) {
+
+     // Find number number clicked
+     //store number
+    //replace display with number
+
+
+}
+
+for ( j = 0; j < operator.length; j++) {
+    //find operator clicked
+    //store operator
+    //add operator to display
+}
+
+function storeNumber(number){
+
+
+}
+
+
+
+var Num1
+var Num2
